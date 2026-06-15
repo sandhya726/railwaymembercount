bfd93d39e7be40df2a21448ea8443953e1c1d44b
sandhya <sandhyareddypottupally@gmail.com>
2026-06-15 14:55:14 +0530

    modified the file

--- count.js
+++ count.js
@@ -1,15 +1,16 @@
 let countEL=document.getElementById("count-el")
 let saveEL=document.getElementById("save-el")
-let count=0;
+let count=0
 function increment()
 {
-    count=count+1;
+    count=count+1
     countEL.innerText=count
 
 }
 function save()
 {
   saveEL.innerText+=" "+count+" - "
-  countEL.innerText=0
-  count=0
+  count=0;
+  countEL.innerText=count;
+  
 }
